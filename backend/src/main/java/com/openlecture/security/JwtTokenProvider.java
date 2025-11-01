cdcccdd0cddcb43fe4c1050bb3bc6b1e12df504f
MeemMMorshed <meemmorshed587@gmail.com>
2025-11-01 13:43:33 -0400

    Working version of login/signup

--- backend/src/main/java/com/openlecture/security/JwtTokenProvider.java
+++ backend/src/main/java/com/openlecture/security/JwtTokenProvider.java
@@ -1,30 +1,42 @@
 package com.openlecture.security;
 
 import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
+
+import java.security.Key;
 import java.util.Date;
 
 @Component
 public class JwtTokenProvider {
-    private final String jwtSecret = "secretKey123";
-    private final long jwtExpirationMs = 86400000; // 1 day
+
+    private final Key jwtSecret = Keys.secretKeyFor(SignatureAlgorithm.HS512); 
+    private final long jwtExpirationMs = 86400000;
 
     public String generateToken(String username) {
         return Jwts.builder()
                 .setSubject(username)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
-                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationMs))
+                .signWith(jwtSecret, SignatureAlgorithm.HS512)
                 .compact();
     }
 
     public String getUsernameFromJwt(String token) {
-        return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
+        return Jwts.parserBuilder()
+                .setSigningKey(jwtSecret)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
     }
 
     public boolean validateToken(String token) {
         try {
-            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
+            Jwts.parserBuilder()
+                    .setSigningKey(jwtSecret)
+                    .build()
+                    .parseClaimsJws(token);
             return true;
         } catch (JwtException | IllegalArgumentException e) {
             return false;
